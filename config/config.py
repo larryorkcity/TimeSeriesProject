69f4f625d20aad63038e403d703363cf5f7e6012
larryorkcity <132366287+larryorkcity@users.noreply.github.com>
2023-09-08 03:19:41 +0900

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,11 @@
+
+# Flask 설정
+SECRET_KEY = 'your_secret_key'
+DEBUG = True
+
+# 데이터베이스 설정
+DB_HOST = 'your_db_host'
+DB_USER = 'your_db_user'
+DB_PASSWORD = 'your_db_password'
+DB_NAME = 'your_db_name'
+    
