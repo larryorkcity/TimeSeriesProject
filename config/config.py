896563e163dc99e744d5790a80e7ae93d2965eab
larryorkcity <132366287+larryorkcity@users.noreply.github.com>
2023-09-09 23:59:33 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,11 +1,11 @@
 
 # Flask 설정
-SECRET_KEY = 'your_secret_key'
+SECRET_KEY = 'test_secret_key'
 DEBUG = True
 
 # 데이터베이스 설정
-DB_HOST = 'your_db_host'
-DB_USER = 'your_db_user'
-DB_PASSWORD = 'your_db_password'
-DB_NAME = 'your_db_name'
+DB_HOST = 'db_host'
+DB_USER = 'ydb_user'
+DB_PASSWORD = 'db_password'
+DB_NAME = 'db_name'
     
